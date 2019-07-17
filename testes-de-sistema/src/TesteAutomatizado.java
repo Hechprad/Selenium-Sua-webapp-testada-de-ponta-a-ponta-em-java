3929e6cffa7f97428fa3705e76becb77d6d2e37d
Jorge Hech <diordiaum@hotmail.com>
2019-07-17 10:48:13 -0300

    first eclipse commit

--- testes-de-sistema/src/TesteAutomatizado.java
+++ testes-de-sistema/src/TesteAutomatizado.java
@@ -1,7 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteAutomatizado {
 
 	public static void main(String[] args) {
-		
+		// WebDriver é a abstração que representa todos os browsers do selenium
+		WebDriver fireFox = new FirefoxDriver();
 	}
 }
