13407a911ea2b36f0f1cb240613066eb70e6d385
Jorge Hech <diordiaum@hotmail.com>
2019-07-17 14:03:38 -0300

    configurando o driver para o navegador chrome

--- testes-de-sistema/src/TesteAutomatizado.java
+++ testes-de-sistema/src/TesteAutomatizado.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteAutomatizado {
 
@@ -22,19 +23,30 @@ public class TesteAutomatizado {
 		 * Utilizamos a linha de código abaixo com o caminho do 
 		 * executável do gecko driver
 		 */
-		System.setProperty("webdriver.gecko.driver", "C:\\Geckodriver\\geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver", "C:\\Drivers-para-SELENIUM\\Geckodriver-firefox\\geckodriver.exe");
 		//**************************************************
+		/*
+		 * O mesmo procedimento para o chrome.
+		 * Chromedriver: download da versão 75.0.3770.90
+		 *  - http://chromedriver.storage.googleapis.com/index.html
+		 */
+		//System.setProperty("webdriver.chrome.driver", "C:\\Drivers-para-SELENIUM\\Chromedriver\\chromedriver.exe");
 		
 		// WebDriver é a abstração que representa todos os browsers do selenium
 		WebDriver driver = new FirefoxDriver();
+		//WebDriver driver2 = new ChromeDriver();
+		
 		// passando a url que iremos testar
 		driver.get("http://www.google.com.br");
+		//driver2.get("http://www.google.com.br");
+		
 		//WebElement representa o objeto que ele encontrou na página
 		WebElement campoDeTexto = driver.findElement(By.name("q"));
+		//WebElement campoDeTexto = driver2.findElement(By.name("q"));
+		
 		// passando valores para o input de busca do google
 		campoDeTexto.sendKeys("Caelum");
-		// submit no formulário para realizar a busca no google
-		campoDeTexto.submit();
-		
+		// submete o formulário para realizar a busca no google
+		campoDeTexto.submit();		
 	}
 }
