16c8d20b97ff8038c3de1f434d869ada01be5a40
Jorge Hech <diordiaum@hotmail.com>
2019-07-17 12:29:24 -0300

    arrumando comentários

--- testes-de-sistema/src/TesteAutomatizado.java
+++ testes-de-sistema/src/TesteAutomatizado.java
@@ -12,20 +12,19 @@ public class TesteAutomatizado {
 		 * criar o caminho do geckodriver nas variaveis de ambiente 
 		 * do windows > variaveis do sistema > path > editar > novo
 		 * 
-		 * Utilizamos a linha de código abaixo com o caminho do executável do gecko driver
+		 * Utilizamos a linha de código abaixo com o caminho do 
+		 * executável do gecko driver
 		 */
 		System.setProperty("webdriver.gecko.driver", "C:\\Geckodriver\\geckodriver.exe");
 
 		// WebDriver é a abstração que representa todos os browsers do selenium
 		WebDriver driver = new FirefoxDriver();
-		
+		// passando a url que iremos testar
 		driver.get("http://www.google.com.br");
-		
-		//WebElemente representa o objeto que ele encontrou na página
+		//WebElement representa o objeto que ele encontrou na página
 		WebElement campoDeTexto = driver.findElement(By.name("q"));
 		// passando valores para o input de busca do google
 		campoDeTexto.sendKeys("Caelum");
-		
 		// submit no formulário para realizar a busca no google
 		campoDeTexto.submit();
 		
