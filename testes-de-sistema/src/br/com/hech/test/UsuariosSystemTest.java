ed0ea48316d92e2ffa9b41366285500c9e593ef5
Jorge Hech <diordiaum@hotmail.com>
2019-07-17 17:31:14 -0300

    nova classe de testes UsuariosSystemTest

--- testes-de-sistema/src/br/com/hech/test/UsuariosSystemTest.java
+++ testes-de-sistema/src/br/com/hech/test/UsuariosSystemTest.java
@@ -1,5 +1,30 @@
 package br.com.hech.test;
 
-public class UsuariosSystemTest {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class UsuariosSystemTest {
+ public static void main(String[] args) {
+	// caminho do chromedriver para utilizar o Google Chrome
+	 System.setProperty("webdriver.chrome.driver", "C:\\Drivers-para-SELENIUM\\Chromedriver\\chromedriver.exe");
+	// driver do chrome
+	WebDriver driver = new ChromeDriver();
+	
+	// passando a url que iremos testar
+	driver.get("http://localhost:8080/usuarios/new");
+
+	// encontrando os inputs pelo nome
+	WebElement nome = driver.findElement(By.name("usuario.nome"));
+	WebElement email = driver.findElement(By.name("usuario.email"));
+	
+	// passando valor para os inputs
+	nome.sendKeys("Jorge Hech");
+	email.sendKeys("jorge@hech.com");
+	
+	// localizando e clicando no botão salvar
+	WebElement botaoSalvar = driver.findElement(By.id("btnSalvar"));
+	botaoSalvar.click();
+}
 }
