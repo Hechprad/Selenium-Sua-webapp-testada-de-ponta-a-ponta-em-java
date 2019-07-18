3ef8f1b1469aec916576e08102a518181ba6e3f0
Jorge Hech <diordiaum@hotmail.com>
2019-07-18 12:06:43 -0300

    new object page NovoLeilao

---
+++ testes-de-sistema/src/br/com/hech/test/NovoLeilaoPage.java
@@ -0,0 +1,35 @@
+package br.com.hech.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class NovoLeilaoPage {
+
+	private WebDriver driver;
+
+	public NovoLeilaoPage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public void preenche(String nome, double valor, String usuario, boolean usado) {
+		WebElement txtNome = driver.findElement(By.name("leilao.nome"));
+		WebElement txtValor = driver.findElement(By.name("leilao.valorInicial"));
+		
+		txtNome.sendKeys(nome);
+		txtValor.sendKeys(String.valueOf(valor));
+		
+		// combo box - select de usuario
+		Select cbUsuario = new Select(driver.findElement(By.name("leilao.usuario.id")));
+		cbUsuario.selectByVisibleText(usuario);
+		
+		// check box de usado
+		if(usado) {
+			WebElement ckUsado = driver.findElement(By.className("leilao.usado"));
+			ckUsado.click();
+		}
+		
+		txtNome.submit();
+	}
+}
