aa098678aa3966a83d2da907fc053771956df832
Jorge Hech <diordiaum@hotmail.com>
2019-07-18 12:17:02 -0300

    new page object LeiloesPage

---
+++ testes-de-sistema/src/br/com/hech/test/LeiloesPage.java
@@ -0,0 +1,29 @@
+package br.com.hech.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LeiloesPage {
+
+	private WebDriver driver;
+
+	public LeiloesPage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public void visita() {
+		driver.get("http://localhost:8080/leiloes");
+	}
+	
+	public NovoLeilaoPage novo() {
+		driver.findElement(By.linkText("Novo Leilão"));
+		return new NovoLeilaoPage(driver);
+	}
+	
+	public boolean existe(String produto, double valor, String usuario, boolean usado) {
+		return driver.getPageSource().contains(produto)
+				&& driver.getPageSource().contains(String.valueOf(valor))
+				&& driver.getPageSource().contains(usuario)
+				&& driver.getPageSource().contains(usado ? "Sim" : "Não");
+	}
+}
